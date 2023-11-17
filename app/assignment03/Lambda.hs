1a4162396fd9ba141e93b298a9ee72b9312241ce
jona leeflang <jona.beer@gmail.com>
2023-11-17 10:48:05 +0100

    Lambda standard working, hidden cases Not yet

--- app/assignment03/Lambda.hs
+++ app/assignment03/Lambda.hs
@@ -7,7 +7,7 @@ import qualified Data.Map.Strict as Map
 import Text.Show.Functions ()
 import Data.Bifunctor (first)
 
-data Token 
+data Token
   = TNum Int
   | TVar String
   | TLambda
@@ -16,15 +16,15 @@ data Token
   | TMult
   deriving (Eq, Show)
 
-data Expr 
+data Expr
   = Num Int
   | Var String
   | Lambda Expr Expr
   | Apply Expr Expr
-  | Mult 
-  | Add 
+  | Mult
+  | Add
   deriving (Eq, Show)
-  
+
 data Value = VInt Int | VFun (Value -> Value)
   deriving (Show)
 
@@ -40,7 +40,7 @@ lexer cs
   | alphanumeric == "mul" = TMult : lexer alphanumericRest
   | alphanumeric /= []    = TVar alphanumeric : lexer alphanumericRest
   | otherwise             = error "LambdaLexer: invalid input"
-  where 
+  where
     (digits, digitsRest) = span isDigit cs
     (alphanumeric, alphanumericRest) = span isLetter cs
 
@@ -48,9 +48,9 @@ lexer cs
 parser :: [Token] -> Expr
 parser tkns = let (e, ts) = parseExpr tkns in if null ts then e
   else error "Parser: Parsing complete, but still tokens left"
-  where 
+  where
     parseExpr :: [Token] -> (Expr, [Token])
-    parseExpr tokens = parseOperators tokens 
+    parseExpr tokens = parseOperators tokens
 
     parseValue :: [Token] -> (Expr, [Token])
     parseValue (TNum n : tokens) = (Num n, tokens) -- Num
@@ -69,7 +69,7 @@ parser tkns = let (e, ts) = parseExpr tkns in if null ts then e
     --   _       -> error "LambdaParser: Var expected after lambda notion"
     --   where (e1, rest) = parseExpr tokens
     -- parseLambda tokens = parseApply tokens
-    
+
     parseLambda :: [Token] -> (Expr, [Token])
     parseLambda (TLambda : tokens) = case e1 of  -- Lambda
       (Var v) -> first (Lambda $ Var v) (parseExpr rest)
@@ -78,7 +78,7 @@ parser tkns = let (e, ts) = parseExpr tkns in if null ts then e
     parseLambda tokens = parseValue tokens
 
     parseApply :: [Token] -> (Expr, [Token]) -- Apply
-    parseApply (TApply : tokens) = (Apply e1 e2, rest') where 
+    parseApply (TApply : tokens) = (Apply e1 e2, rest') where
       (e1, rest ) = parseExpr tokens
       (e2, rest') = parseExpr rest
     parseApply tokens = parseLambda tokens
@@ -89,55 +89,42 @@ parser tkns = let (e, ts) = parseExpr tkns in if null ts then e
     parseOperators tokens           = parseApply tokens
 
 -- | Evaluates an expression into a Value
+-- | Substitute: Base case String is empty (cannot match variables) 
 eval :: Expr -> Value
-eval = eval' Map.empty . substitute "" (Num $ -1) 
-  -- Empty string will never match vars, thus placeholder
-  -- Becaue empty string will never match the given expr will never be bound, also placeholder (-1, could indicate error in output to user)
-  
+eval = eval' Map.empty .substitute
+
   where
     eval' :: Map String Value -> Expr -> Value
     eval' _   (Num x)                           = VInt x
     eval' env (Var v)                           = env ! v
     eval' _   Add                               = VFun $ VFun . (|+|)
-    eval' _   Mult                              = VFun $ VFun . (|*|) 
+    eval' _   Mult                              = VFun $ VFun . (|*|)
     eval' env (Apply Add e2)                    = VFun (eval' env e2 |+|)
     eval' env (Apply Mult e2)                   = VFun (eval' env e2 |*|)
     eval' env (Apply (Lambda (Var var) body) e2)= eval' (Map.insert var (eval' env e2) env) body
     eval' env (Lambda (Var var) body)           = VFun $ \f -> eval' (Map.insert var f env) body
-    eval' env (Apply e1 e2)                     = case (eval' env e1) of
+    eval' env (Apply e1 e2)                     = case eval' env e1 of
       (VFun f) -> f (eval' env e2)
       _ -> error "Cannot apply value like a function to anothe value"
     eval' env e                                   = error $ "Eval: Error during evaluation, not matched pattern :: " ++ show e ++ "::ENV:: " ++ show env
 
--- | Substitutes variables in expression. 
+-- | Substitutes expressions applied to expressions by 'inlining' 
 -- |  i.e. lambda applied to another expression replaces the lambda variables occurences with the expression
-substitute :: String -> Expr -> Expr -> Expr 
-substitute _ _ (Num x)                           = (Num x)
-substitute s e (Var var)                         = if var == s then e else (Var var)
-substitute _ _ Add                               = Add
-substitute _ _ Mult                              = Mult
-substitute s e (Apply (Lambda (Var v) body) e2)  = substitute v (substitute s e e2) body
-substitute s e (Apply e1 e2)                     = Apply (substitute s e e1) (substitute s e e2)  
-substitute s e (Lambda (Var v) body)             = Lambda (Var v) (substitute s e body)
-substitute _ _ _                                 = error "Substitution: Error occured during substitution of lambda abstractions"
-
--- TODO, perhaps the x's overlap eachother in "`#f `#x `f 2 10 `#x #y ``add x y 3"
---  As in, right hand lambda is inlined, then \x -> \x->x \y ->, when the first x should be ignored but now maybe breaking
-
-
--- Apply     (Lambda (Var "x")(Lambda (Var "y") (Apply (Apply  Add  (Var "x") ) (Var "y") )))      (Num 3)
--- Apply 
---   (Lambda (Var "x") 
---     (Lambda (Var "y") 
---       (Apply 
---         (Apply 
---           Add 
---           (Num 3)
---         ) 
---         (Var "y")
---       )
---     )
---   ) (Num 3)
+--    Empty string will never match vars, thus placeholder
+--    Becaue empty string will never match the given expr will never be bound, also placeholder (-1, could indicate error in output to user)
+substitute :: Expr -> Expr
+substitute = substitute' "" (Num $ -1)
+  where 
+    -- | Inner substitution function, replaces variables of given string with expression
+    substitute' :: String -> Expr -> Expr -> Expr
+    substitute' _ _ (Num x)                           = Num x
+    substitute' s e (Var var)                         = if var == s then e else Var var
+    substitute' _ _ Add                               = Add
+    substitute' _ _ Mult                              = Mult
+    substitute' s e (Apply (Lambda (Var v) body) e2) = substitute' v (substitute' s e e2) (substitute' s e body) -- substitute' the lambda body before total substitution to work away duplicate variables
+    substitute' s e (Apply e1 e2)                     = Apply (substitute' s e e1) (substitute' s e e2)
+    substitute' s e (Lambda (Var v) body)             = Lambda (Var v) (substitute' s e body)
+    substitute' _ _ _                                 = error "Substitution: Error occured during substitution of lambda abstractions"
 
 -- | Operators for runOp of addition and multiplication
 (|+|), (|*|) :: Value -> Value -> Value
@@ -146,11 +133,11 @@ substitute _ _ _                                 = error "Substitution: Error oc
 
 -- | Runs an binary integer operator on the Value wrapper
 runOp:: (Int -> Int -> Int) -> Value -> Value -> Value
-runOp op e1 e2 = case (e1, e2) of 
+runOp op e1 e2 = case (e1, e2) of
   (VInt i, VInt j) -> VInt $ op i j
   (VFun f, VInt j) -> VFun $ \val -> runOp op (f val)  (VInt j)
-  (VInt i, VFun g) -> VFun $ \val -> runOp op (VInt i) (g val) 
-  (VFun f, VFun g) -> VFun $ \val -> VFun (runOp op (f val) . g) 
+  (VInt i, VFun g) -> VFun $ \val -> runOp op (VInt i) (g val)
+  (VFun f, VFun g) -> VFun $ \val -> VFun (runOp op (f val) . g)
   -- (VFun f, VFun g) -> VFun $ VFun . (runOp op) . g . f
 
 -- | Parser function used for debugging purposes
@@ -159,81 +146,10 @@ pp (Apply e1 e2)        = '(' : pp e1 ++ ") (" ++ pp e2 ++ ")"
 pp (Lambda (Var v) e2)  = "_" ++ v ++ " -> {" ++ pp e2 ++ "}"
 pp (Num i)              = show i
 pp (Var v)              = v
-pp (Add)                = "add"
-pp (Mult)               = "mul"
+pp Add                = "add"
+pp Mult               = "mul"
 pp _                    = "<Some invalid>"
 
 -- | Combines evaluation, parsing and lexing
 interpret :: String -> Value
 interpret  = eval . parser . lexer
-
-
--- `#f `#x `f 2 10 `#x #y ``add x y 3
-
--- Error raised: Map.!: given key is not an element in the map \n CallStack (from HasCallStack): 
--- \n error, called at libraries/containers/containers/src/Data/Map/Internal.hs:617:17 in containers-0.6.7:Data.Map.Internal
-ll = (Lambda (Var "f")(Apply  (Lambda (Var "x")(Apply (Var "f") (Num 2)   ) )  (Num 10)) ) 
-rr = (Apply(Lambda (Var "x")  (Lambda (Var "y")(Apply (Apply   Add   (Var "x")) (Var "y")   ) ))(Num 3)  )
-
-bb = Apply ll rr
--- (Apply   (Lambda (Var "f")(Apply  (Lambda (Var "x")(Apply (Var "f") (Num 2)   ) )  (Num 10))  )   (Apply(Lambda (Var "x")  (Lambda (Var "y")(Apply (Apply   Add   (Var "x")) (Var "y")   ) ))(Num 3)  ))
---  ===
--- Apply 
---   (Lambda (Var "f") 
---     (Apply 
---       (Lambda (Var "x") 
---         (Apply 
---           (Var "f") 
---           (Num 2)
---         )
---       ) 
---       (Num 10)
---     )
---   ) 
---   (Apply 
---     (Lambda (Var "x") 
---       (Lambda (Var "y") 
---         (Apply 
---           (Apply 
---             Add 
---             (Var "x")
---           ) 
---           (Var "y")
---         )
---       )
---     ) 
---     (Num 3)
---   )
-
--- (Apply (Lambda (Var "f") (Apply (Var "f") (Num 2)  ) )  (Lambda (Var "y")   (Apply  (Apply   Add  (Num 3) ) (Var "y")) ))
---  ===
--- Apply 
---   (Lambda (Var "f") 
---     (Apply 
---       (Var "f") 
---       (Num 2)
---     )
---   ) 
---   (Lambda (Var "y") 
---     (Apply 
---       (Apply 
---         Add 
---         (Num 3)
---       ) 
---       (Var "y")
---     )
---   )
-
-
--- (Apply 
---   (Lambda (Var "y") 
---     (Apply 
---       (Apply 
---         Add 
---         (Num 3)
---       ) 
---       (Var "y")
---     )
---   ) 
---   (Num 2)
--- )
